30f4130dfbaf3fe858c116d5b0967857ede2098a
chisoftltd <chisoftltd@gmail.com>
2020-05-19 01:35:38 +0200

    More Commits

---
+++ DataAccessLibrary/CustomerData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary
+{
+    public class CustomerData
+    {
+        private readonly ISqlDataAccess _db;
+
+        public CustomerData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+    }
+}
---
+++ DataAccessLibrary/Models/CustomerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.Models
+{
+    public class CustomerModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
